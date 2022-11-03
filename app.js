a7899058f4c0117f2ef1edb3740bd09ca5686f3e
victor <victorbuquera@gmail.com>
2022-11-02 21:12:08 -0300

    FInalizado

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+
 const app = Vue.createApp({
     data(){
         return {
@@ -14,6 +15,11 @@ const app = Vue.createApp({
             const { results } = await res.json()
             
             console.log(results)
+            this.firstName = results[0].name.first
+            this.lastName = results[0].name.last
+            this.email = results[0].email            
+            this.gender = results[0].gender    
+            this.picture = results[0].picture.large
         }
     }
 })
